9261646e7468e0d31a30c61ea158c7a480d37606
nruffin95 <nruffin95@gmail.com>
2022-04-07 17:28:56 -0500

    Added Screen Shot

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,10 +30,11 @@ function generatePassword(){
     function writePassword() {
       var password = generatePassword();
       var passwordText = document.querySelector("#password");
-    
+      
       passwordText.value = password;
-    
+        
     }
     
     // Add event listener to generate button
     generateBtn.addEventListener("click", writePassword);
+
